a42f564dffdab0d48181ef45337a98e9eadb618a
SeanCena <seansng1@gmail.com>
2023-04-19 14:26:25 -0700

    Completed item 4

--- src/MyApp.js
+++ src/MyApp.js
@@ -8,18 +8,23 @@ function MyApp() {
     const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter(index) {
-        const updated = characters.filter((character, i) => {
-            return i !== index;
+        const id = characters[index]["id"];
+        makeDeleteCall(id).then((result) => {
+            if (result && result.status === 204) {
+                const updated = characters.filter((character, i) => {
+                    return i !== index;
+                });
+                setCharacters(updated);
+            }
         });
-        setCharacters(updated);
     }
 
     function updateList(person) { 
         makePostCall(person).then( result => {
         if (result && result.status === 201)
-           setCharacters([...characters, person] );
+            setCharacters([...characters, person] );
         });
-     }
+    }
 
     async function fetchAll(){
         try {
@@ -36,6 +41,17 @@ function MyApp() {
     async function makePostCall(person){
         try {
             const response = await axios.post('http://localhost:8000/users', person);
+            return response;
+        }
+        catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async function makeDeleteCall(id){
+        try {
+            const response = await axios.delete('http://localhost:8000/users/' + id);
             return response;
         }
         catch (error) {
